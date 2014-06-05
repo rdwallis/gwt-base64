6900227ca0a4c2cff6059fd8bec84681cfbcf34d
Richard Wallis <rdwallis@gmail.com>
2014-06-05 11:18:49 +0200

    first commit

---
+++ src/main/java/com/wallissoftware/base64/client/Base64Native.java
@@ -0,0 +1,18 @@
+package com.wallissoftware.base64.client;
+
+import com.google.inject.Singleton;
+
+@Singleton
+public class Base64Native implements Base64 {
+
+    @Override
+    public native final String decode(final String base64) /*-{
+		return $wnd.atob(base64);
+	}-*/;
+
+    @Override
+    public native final String encode(final String input) /*-{
+		return $wnd.btoa(input);
+	}-*/;
+
+}
